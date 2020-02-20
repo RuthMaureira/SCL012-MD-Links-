fac2f8b1a2f3a78c875faa1582b9bc24b1c9a13b
Ruth Maureira <ruthalemau@gmail.com>
2020-02-20 20:17:27 -0300

    modificaciones cuando es directorio

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
+const libPath = require('path');
+
 const fileLinks = require('./lib/links.js');
+const fileValidate = require('./lib/validate.js');
+const fileStats = require('./lib/stats.js');
 const chalk = require('chalk');
+const fetch = require('node-fetch');
 
 /* const arg = process.argv; */
 const path = process.argv[2];
@@ -20,46 +25,161 @@ const options = {
    console.log(arg3); 
 */
 
+let totalLinks = 0;
+
 const mdLinks = (path, options) => {
 
-  const inputOk = fileLinks.checkArg(path, options.validate, options.stats);
-  console.log(inputOk);
-  const absoluteOk = fileLinks.pathIsAbsolute(inputOk[0]);
-  console.log(absoluteOk);
+  let input;
+  let linksTotal;
 
-  const extension = fileLinks.checkExt(absoluteOk);
-  console.log(extension);
+  let promise = new Promise(function (resolve, reject) {
+    const inputOk = fileLinks.checkArg(path, options.validate, options.stats);
+    /* console.log(inputOk); */
+    resolve(inputOk)
+  });
 
-  const stat = fs.statSync(absoluteOk);
+  promise.then(function (inputOk) {
+      const absoluteOk = fileLinks.pathIsAbsolute(inputOk[0]);
+      input = [absoluteOk, inputOk]
+      /* console.log(absoluteOk); */
+      return input;
+    })
 
-  if(stat.isDirectory() === true){
+    .then(function (input) {
 
-  }
-  
+      /* const extension = fileLinks.checkExt(absoluteOk);
+      console.log(extension); */
+      absoluteOk = input[0];
+      inputOk = input[1];
+      /* console.log(inputOk); */
+      let filesDirectory;
+      let countTotal;
+      options = {
+        validate: inputOk[1],
+        stats: inputOk[2]
+      }
 
-  else if(stat.isFile() === true) {
-    
-    if (extension === '.md') {
+      /* console.log(options); */
+      const stat = fs.statSync(absoluteOk);
 
-      fileLinks.readNewFile(absoluteOk)
-        .then(function (res) {
-          console.log(res);
+      let pathFile = [];
+      let count = 0;
 
-          /* Rescatar links del archivo */
-         
+      if (stat.isDirectory() === true) {
 
-        })
+        /* Análisis sobre el directorio inmediato */
+
+        fs.readdir(absoluteOk, (err, files) => {
+          files.forEach((file) => {
+            if (libPath.extname(file) === '.md') {
+              pathFile[count] = file;
+              count++;
+              mdLinks(file, options);
+            }
+          });
+
+          if (count === 0) {
+            console.log('En directorio ingresado no hay archivos con extensión md');
+          } else {
+            console.log('Total de archivos md en directorio ingresado: ' + chalk.red.bold(count));
+            console.log(pathFile);
+          }
+          /* console.log(pathFile); */
+          return count;
+        });
+
+      } else if (stat.isFile() === true) {
+
+        if (libPath.extname(absoluteOk) === '.md') {
+
+          /* Expresión regular para rescatar links */
+          const regexMdLinks = /\[([^\[]+)\](\(.*\))/gm;
+          /* Expresión regular para separar texto de href */
+          const singleMatch = /\[([^\[]+)\]\((.*)\)/;
+
+          let textLinks;
+          let arrLinks = [];
+
+          let readNewFile = (absoluteOk) => {
+            return new Promise((resolve, reject) => {
+              fs.readFile(absoluteOk, "utf8", (error, data) => {
+                if (error) return reject(new Error('No es un archivo válido'))
+                resolve(data);
+              });
+            })
+          }
+          readNewFile(absoluteOk).then(data => {
+
+            const markdown = data.toString();
+            const matches = markdown.match(regexMdLinks);
+            let total;
+
+            if (matches === null) {
+              total = 0;
+              console.log('No hay links en archivo md que analizar')
+            } else {
+
+              let countBroken = 0;
+              total = matches.length;
+
+              console.log(chalk.blue.bold('El archivo ' + libPath.basename(absoluteOk) + ' tiene ' + total + ' links'));
+
+              for (let i = 0; i < matches.length; i++) {
+                textLinks = singleMatch.exec(matches[i]);
+                /* console.log(textLinks); */
+                arrLinks[i] = {
+                  href: textLinks[2],
+                  text: textLinks[1].slice(0, 50),
+                  file: absoluteOk
+                };
+              }
+              console.log(arrLinks);
+              /*newUnique = [...new Set(link)];
+              console.log(countBroken);
+              return arrLinks, countBroken;*/
+              console.log(total)
+              return total;
+              
+            }
+          })
+          /*.catch(error => console.error(chalk.red(error)));*/
+        } else {
+          console.log(chalk.red.bold('Su archivo no es md'));
+        }
+      }
+
+    })
+
+  /*.then(linksTotal => {
+    console.log(linksTotal);
+    console.log('Cantidad de archivos md encontrados en directorios: ' + linksTotal[1]);
+  })*/
+
+
+
+
+
+
+
+  /* if (stat.isDirectory() === true) {
+
+   } else if (stat.isFile() === true) {
+
+     if (extension === '.md') {
+
+       fileLinks.readNewFile(absoluteOk)
+         .then(function (res) {
+           console.log(res);
+
+           /* Rescatar links del archivo */
+
+
+  /*   })
     }
-  } else {
-  }
+  } else {}*/
 
 
 }
 
 mdLinks(path, options);
-
-
-
-
-
-
+exports.mdLinks = mdLinks;
