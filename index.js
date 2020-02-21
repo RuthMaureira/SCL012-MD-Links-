7ef390d1d96cf562ce2dd07079cfc064191ab6f8
Ruth Maureira <ruthalemau@gmail.com>
2020-02-20 21:03:25 -0300

    Guardar href, text y file

--- index.js
+++ index.js
@@ -110,76 +110,146 @@ const mdLinks = (path, options) => {
           }
           readNewFile(absoluteOk).then(data => {
 
-            const markdown = data.toString();
-            const matches = markdown.match(regexMdLinks);
-            let total;
+              const markdown = data.toString();
+              const matches = markdown.match(regexMdLinks);
+              let total;
+              let totalLinks;
 
-            if (matches === null) {
-              total = 0;
-              console.log('No hay links en archivo md que analizar')
-            } else {
+              if (matches === null) {
+                total = 0;
+                console.log('No hay links en archivo md que analizar')
+              } else {
 
-              let countBroken = 0;
-              total = matches.length;
+                let countBroken = 0;
+                total = matches.length;
 
-              console.log(chalk.blue.bold('El archivo ' + libPath.basename(absoluteOk) + ' tiene ' + total + ' links'));
+                console.log(chalk.blue.bold('El archivo ' + libPath.basename(absoluteOk) + ' tiene ' + total + ' links'));
 
-              for (let i = 0; i < matches.length; i++) {
-                textLinks = singleMatch.exec(matches[i]);
-                /* console.log(textLinks); */
-                arrLinks[i] = {
-                  href: textLinks[2],
-                  text: textLinks[1].slice(0, 50),
-                  file: absoluteOk
-                };
+                for (let i = 0; i < matches.length; i++) {
+                  textLinks = singleMatch.exec(matches[i]);
+                  /* console.log(textLinks); */
+                  arrLinks[i] = {
+                    href: textLinks[2],
+                    text: textLinks[1].slice(0, 50),
+                    file: absoluteOk
+                  };
+                }
+                console.log(arrLinks);
+                /*newUnique = [...new Set(link)];
+                console.log(countBroken);
+                return arrLinks, countBroken;*/
+                console.log(total)
+                totalLinks = [arrLinks, total];
+                return totalLinks;
+
               }
-              console.log(arrLinks);
-              /*newUnique = [...new Set(link)];
+            })
+            .catch(error => console.error(chalk.red(error)))
+
+
+            .then(totalLinks => {
+              console.log(totalLinks);
+
+              const arrLinks2 = totalLinks[0];
+              const arrTotal = totalLinks[1];
+              const hrefLinks = arrLinks2[0].href;
+              console.log(arrLinks2);
+              console.log(hrefLinks);
+              console.log(arrTotal);
+              console.log(options.validate);
+              let countBroken = 0;
+
+              if (arrTotal > 0 && options.validate === true) {
+               
+                for (let i = 0; i < arrTotal; i++) {
+                  fetch(arrLinks2[i].href)
+                    .then(res => {
+                      /* console.log(res.ok);
+                       console.log(res.status);
+                       console.log(res.statusText);
+                       console.log(res.headers.raw());
+                      console.log(res.headers.get('content-type'));*/
+                      arrLinks2[i].status = res.status;
+                      if (res.ok === true) {
+                        arrLinks2[i].statusText = 'ok';
+                      } else {
+                        arrLinks2[i].statusText = 'fail';
+                        countBroken++;
+                      }
+                    })
+                }
+              }
+
+
+              /*arrLinks2.forEach((link) => {
+                fetch()
+                .then(res => {
+                  /* console.log(res.ok);
+                   console.log(res.status);
+                   console.log(res.statusText);
+                   /* console.log(res.headers.raw());
+                    console.log(res.headers.get('content-type'));*/
+              /*  arrLinks.status = res.status;
+                  if (res.ok === true) {
+                    arrLinks.statusText = 'ok';
+                  } else {
+                    arrLinks.statusText = 'fail';
+                    countBroken++;
+                  }
+              })*/
+
+              /*console.log(arrLinks);
+              newUnique = [...new Set(link)];
               console.log(countBroken);
               return arrLinks, countBroken;*/
-              console.log(total)
-              return total;
-              
-            }
-          })
-          /*.catch(error => console.error(chalk.red(error)));*/
+            })
+            .catch(err => console.error(chalk.red(err)));
+
         } else {
           console.log(chalk.red.bold('Su archivo no es md'));
         }
       }
 
+
+
+
+
+
     })
-
-  /*.then(linksTotal => {
-    console.log(linksTotal);
-    console.log('Cantidad de archivos md encontrados en directorios: ' + linksTotal[1]);
-  })*/
-
-
-
-
-
-
-
-  /* if (stat.isDirectory() === true) {
-
-   } else if (stat.isFile() === true) {
-
-     if (extension === '.md') {
-
-       fileLinks.readNewFile(absoluteOk)
-         .then(function (res) {
-           console.log(res);
-
-           /* Rescatar links del archivo */
-
-
-  /*   })
-    }
-  } else {}*/
-
-
 }
+
+
+
+/*.then(linksTotal => {
+  console.log(linksTotal);
+  console.log('Cantidad de archivos md encontrados en directorios: ' + linksTotal[1]);
+})*/
+
+
+
+
+
+
+
+/* if (stat.isDirectory() === true) {
+
+ } else if (stat.isFile() === true) {
+
+   if (extension === '.md') {
+
+     fileLinks.readNewFile(absoluteOk)
+       .then(function (res) {
+         console.log(res);
+
+         /* Rescatar links del archivo */
+
+
+/*   })
+  }
+} else {}*/
+
+
+
 
 mdLinks(path, options);
 exports.mdLinks = mdLinks;
